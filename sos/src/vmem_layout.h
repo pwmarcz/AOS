317a141d11af0fe3b342fce0aecdf9defd1cfcf5
Paweł Marczewski <pwmarcz@gmail.com>
2022-01-04 15:43:59 +0100

    Adjust memory layout for 32-bit

--- sos/src/vmem_layout.h
+++ sos/src/vmem_layout.h
@@ -22,9 +22,9 @@
 #define SOS_STACK            (0xC0000000)
 #define SOS_IPC_BUFFER       (0xD0000000)
 #define SOS_STACK_PAGES      100
-#define SOS_UT_TABLE         (0x8000000000)
-#define SOS_FRAME_TABLE      (0x8100000000)
-#define SOS_FRAME_DATA       (0x8200000000)
+#define SOS_UT_TABLE         (0x80000000)
+#define SOS_FRAME_TABLE      (0x81000000)
+#define SOS_FRAME_DATA       (0x82000000)
 
 /* Constants for how SOS will layout the address space of any processes it loads up */
 #define PROCESS_STACK_TOP   (0x90000000)
